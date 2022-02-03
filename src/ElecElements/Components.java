ab5052b84333d08329ec6667da54abcc29067be4
Abdulkhamid <abdulkhamidov_aa@orlan.in>
2022-02-03 10:23:16 +0300

    Added an approximate structure part of the code.

---
+++ src/ElecElements/Components.java
@@ -0,0 +1,13 @@
+package ElecElements;
+
+public class Components {
+
+    private Object component;
+    private String[] partNumbers;
+    private int id;
+    private String codeComponent; //Зашифраованный код компонента по своим правилам
+
+    public Components(String partNumber) {
+
+    }
+}
